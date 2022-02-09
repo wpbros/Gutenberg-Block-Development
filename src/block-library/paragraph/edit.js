03310bc2475b2699fadff074749a3016e6f67366
Phil Sola <philsola88@gmail.com>
2022-02-09 08:44:59 +0000

    Episode 6

--- src/block-library/paragraph/edit.js
+++ src/block-library/paragraph/edit.js
@@ -1,20 +1,51 @@
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { useBlockProps, RichText, InspectorControls } from '@wordpress/block-editor';
+import { Panel, PanelBody, PanelRow, ColorPalette } from '@wordpress/components';
 
 import './editor.scss';
 
-export default function Edit({ attributes: { content }, setAttributes }) {
+export default function Edit({ attributes: { content, textColor, backgroundColor }, setAttributes }) {
 	
 	function editContentHandler(newVal) {
 		setAttributes({ content: newVal });
 	}
 	
 	return (
-		<RichText 
-			{ ...useBlockProps() }
-			value={content}
-			onChange={editContentHandler}
-			tagName='p'
-			placeholder='Enter text here.'
-		/>
+		<>	
+			<InspectorControls>
+				<panelBody title="Color Settings">
+					<p>Text Color</p>
+					<ColorPalette 
+						colors={[
+							{ name: 'Red', color: '#f00' },
+							{ name: 'White', color: '#fff' },
+							{ name: 'Blue', color: '#00f' },
+						]}
+						value={ textColor }
+						onChange={ val => setAttributes({ textColor: val }) }
+					/>
+					
+					<br/>
+					
+					<p>Background Color</p>
+					<ColorPalette 
+						colors={[
+							{ name: 'Red', color: '#f00' },
+							{ name: 'White', color: '#fff' },
+							{ name: 'Blue', color: '#00f' },
+						]}
+						value={ backgroundColor }
+						onChange={ val => setAttributes({ backgroundColor: val }) }
+					/>					
+				</panelBody>
+			</InspectorControls>
+			
+			<RichText 
+				{ ...useBlockProps({ style: { color: textColor, backgroundColor: backgroundColor } }) }
+				value={content}
+				onChange={editContentHandler}
+				tagName='p'
+				placeholder='Enter text here.'
+			/>
+		</>
 	);
 }
--- src/block-library/paragraph/save.js
+++ src/block-library/paragraph/save.js
@@ -2,10 +2,10 @@ import { useBlockProps, RichText } from "@wordpress/block-editor";
 
 import "./editor.scss";
 
-export default function Edit( { attributes: { content } } ) {
+export default function Edit( { attributes: { content, textColor, backgroundColor } } ) {
     return (
         <RichText.Content 
-            { ...useBlockProps.save() } 
+            { ...useBlockProps.save({ style: { color: textColor, backgroundColor: backgroundColor }, className: 'color-block' }) } 
             tagName="p"
             value={content}
         />
